08f502df0f62df420c57b835033019b77fb5e99a
ENGO150 <v.smejkal06@gmail.com>
2024-02-04 11:50:27 +0100

    added comments

--- src/main.rs
+++ src/main.rs
@@ -31,39 +31,39 @@ fn main()
 
 pub fn colorize_img(path_in: &str, path_out: &str)
 {
-    if Path::new(path_out).exists() { remove_file(path_out).expect("File deletion failed!"); }
-    let mut img = image::open(path_in).expect("File not found!");
+    if Path::new(path_out).exists() { remove_file(path_out).expect("File deletion failed!"); } //REMOVE DUPLICATE FRAMES
+    let mut img = image::open(path_in).expect("File not found!"); //LOAD ORIGINAL
 
     let (w, h) = img.dimensions();
     let mut output: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(w, h);
     let mut negative: bool;
 
-    img = img.blur(1.5);
+    img = img.blur(1.5); //BLUR THAT
 
-    for (x, y, pixel) in img.pixels()
+    for (x, y, pixel) in img.pixels() //LOOP TROUGH PIXELS
     {
         let Rgba([red, green, blue, alpha]) = pixel;
 
         negative = thread_rng().gen_range(0..(img.width() * img.height() / 480)) == 69;
 
-        let px = Rgba([((distort(red, negative) as i32 - 19).abs() / 2) as u8, ((distort(green, negative) as i32 - 70).abs() / 2) as u8, ((distort(blue, negative) as i32 - 22).abs() / 2) as u8, alpha]);
+        let px = Rgba([((distort(red, negative) as i32 - 19).abs() / 2) as u8, ((distort(green, negative) as i32 - 70).abs() / 2) as u8, ((distort(blue, negative) as i32 - 22).abs() / 2) as u8, alpha]); //COMPLETELY FUCK THE COLORS
 
         output.put_pixel
         (
             x,
             y,
             px.map(|p| 255 - ((p.saturating_pow(4) as f64) / 2.) as u8),
-        );
+        ); //ADD TO OUTPUT
     }
 
-    output.save(path_out).expect("Saving failed!");
+    output.save(path_out).expect("Saving failed!"); //SAVE OUTPUT
 }
 
-fn distort(p: u8, negative: bool) -> u8
+fn distort(p: u8, negative: bool) -> u8 //FUCK THE IMAGE
 {
     let mut returning = if p != 0
     {
-        (thread_rng().gen_range((p as f64 * 70.)..(p as f64 * 140. + 1.)) / 100.).floor() as u8 % 255
+        (thread_rng().gen_range((p as f64 * 70.)..(p as f64 * 140. + 1.)) / 100.).floor() as u8 % 255 //GENERATE RANDOM COLOR AROUND THE p
     } else { 0 };
 
     returning = (returning as f32 * 1./4.) as u8;
